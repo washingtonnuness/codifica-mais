7fc269ae24300e3e7c0ecb5f3f00e7bda6994fb8
Admin <washingtonnuness@gmail.com>
2025-09-25 20:58:48 -0300

    Iniciando exercicio a7

--- PHP-CONCEITOS/wn-cod-php-a7.php
+++ PHP-CONCEITOS/wn-cod-php-a7.php
@@ -14,7 +14,11 @@
 //     "preco" => 69.90,
 
 // ];
-$pessoas = readline("Numero pessoas");
+
+
+
+$pessoas = 20;
+//$pessoas = readline("Numero pessoas");
 
 $precoProdutos = [
     19.90,
@@ -32,9 +36,29 @@ foreach ($precoProdutos as $preco) {
 
 echo $valorTotal . PHP_EOL;
 
-$controle = count($precoProdutos);
+//$controle = count($precoProdutos);
 
 // for ($cont = 0; $cont < $controle; $cont++) {
 //     $valorTotal = $precoProdutos[$cont];
 //     echo "Preco itens $precoProdutos[$cont]" . PHP_EOL;
 // }
+
+
+function calcXurras($pessoas, $valorTotal)
+{
+    $itensComprados = [
+        "produto" => "Queijo",
+        "produto" => "Costela",
+        "produto" => "Faldinha",
+        "produto" => "Picanha"
+    ];
+    $vaquinha = $valorTotal / $pessoas;
+    foreach ($itensComprados as $produto) {
+        print_r("Itens do churrasco .: $produto" . PHP_EOF);
+    }
+    echo "Valor total das compras foi de .: $valorTotal" . PHP_EOL;
+
+    echo "O valor por cabeça é de .: $vaquinha" . PHP_EOL;
+}
+
+calcXurras($pessoas, $valorTotal);
